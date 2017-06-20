0a83ace42f723f03b1b1f0e61a9a352215792a05
Alexey Gerasev <alexey.gerasev@gmail.com>
2017-06-21 03:14:24 +0700

    jquery and mouse

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-function ajax(path, ok, err) {
-	let req = new XMLHttpRequest();
-	if (!req) {
-		err(null);
-		return null;
+tools = {}
+
+tools.ajax = function (path, ok, err) {
+	$.ajax(path).done(ok).error(err);
+}
+
+tools.mousepos = function (e) {
+	var pos = [0, 0];
+	if(e.offsetX) {
+		pos[0] = e.offsetX;
+		pos[1] = e.offsetY;
+	} else if(e.layerX) {
+		pos[0] = e.layerX;
+		pos[1] = e.layerY;
 	}
-	req.onreadystatechange = function() {
-		if (this.readyState == XMLHttpRequest.DONE) {
-			if (this.status == 200) {
-				ok && ok(this.responseText);
-			} else {
-				err && err(this.status);
-			}
-		}
-	};
-	req.open("GET", path, true);
-	req.send();
-	return req;
+	return pos;
+}
+
+tools.Mouse = function (elem) {
+	var self = this;
+	self.elem = elem;
+
+	self.pos = [0, 0];
+	self.left = false;
+	self.middle = false;
+	self.right = false;
+
+	self.elem.addEventListener("mousemove", function(e) {
+		var mp = tools.mousepos(e);
+		self.pos[0] = mp[0];
+		self.pos[1] = mp[1];
+		self.move(self.pos);
+	});
+
+	self.elem.addEventListener("wheel", function(e) {
+		var dz = (e.deltaY > 0) - (e.deltaY < 0);
+		self.wheel(dz);
+	});
 }
